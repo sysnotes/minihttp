c219aa7689a0941adba37d29eb77d11453c58381
SreeRaj-K0 <cybersec221@gmail.com>
2026-09-08 23:00:29 +0530

    stage 02: bind socket to port 8080

--- server.c
+++ server.c
@@ -8,12 +8,28 @@ int main(void)
 {
     int server_fd;
 
+    struct sockaddr_in address;
+
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (server_fd == -1) {
         perror("socket");
         return 1;
     }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
+    perror("bind");
+    close(server_fd);
+    return 1;
+}
+
+
+
 
     printf("Socket created: %d\n", server_fd);
 
